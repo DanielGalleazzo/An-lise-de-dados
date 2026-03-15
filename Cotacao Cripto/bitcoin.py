3393c24923d858ba80e56801eaa4f5ba0774c180
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2026-03-15 18:43:00 -0300

    Criação de grafico para o bitcoin

--- Cotacao Cripto/bitcoin.py
+++ Cotacao Cripto/bitcoin.py
@@ -1,5 +1,8 @@
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import polars as pl
 
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 
@@ -13,5 +16,13 @@ data = response.json()
 df = pd.DataFrame(data["prices"], columns=["timestamp", "price"])
 df["date"] = pd.to_datetime(df["timestamp"], unit="ms")
 df = df.set_index("date")
+
 df_month = df["price"].resample("M").last()
 print(df_month)
+
+
+plt.title("Bitocoin 2025-2026")
+plt.xlabel("Mês")
+plt.ylabel("Valor em USD")
+plt.bar(df_month.index, df_month.values)
+plt.show()
