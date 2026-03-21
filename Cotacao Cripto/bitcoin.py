2d67e7c5f05bf11e4a2e60876b9716fa99eb8a44
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2026-03-21 01:11:08 -0300

    Update bitcoin.py
    metodo que compara duas moedas

--- Cotacao Cripto/bitcoin.py
+++ Cotacao Cripto/bitcoin.py
@@ -1,28 +1,48 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-import polars as pl
-moeda = "bitcoin"
-url = "https://api.coingecko.com/api/v3/coins/"+moeda+"/market_chart"
 
-params = {
-    "vs_currency": "usd",
-    "days": "365"
-}
+def moedaConsulta () :
+    moeda1 = "solana"
+    moeda2 = "ethereum"
+    url1 = "https://api.coingecko.com/api/v3/coins/" + moeda1 + "/market_chart"
+    params1 = {
+        "vs_currency": "usd",
+        "days": "60"
+    }
+    url2 = "https://api.coingecko.com/api/v3/coins/" + moeda2 + "/market_chart"
+    params2 = {
+        "vs_currency": "usd",
+        "days": "60"
+    }
+    response1 = requests.get(url1, params=params1)
+    response2 = requests.get(url2, params=params2)
+    data1 = response1.json()
+    data2 = response2.json()
+    df1 = pd.DataFrame(data1["prices"], columns=["timestamp", "price"])
+    df2 = pd.DataFrame(data2["prices"], columns=["timestamp","price"])
+    df1["date"] = pd.to_datetime(df1["timestamp"], unit="ms")
+    df2["date"] = pd.to_datetime(df2["timestamp"], unit="ms")
+    df1 = df1.set_index("date")
+    df2 = df2.set_index("date")
+    df_date1 = df1["price"].resample("W").last()
+    df_date2 = df2["price"].resample("W").last()
+    print(df_date1)
+    print(df_date2)
+    plt.title(moeda1 + " and " + moeda2 + " 2025-2026")
+    plt.xlabel("Mês")
+    plt.ylabel("Valor em USD")
+    x = df_date1.index
+    plt.bar(x - pd.Timedelta(days=2), df_date1.values, 
+    width=3, color="red", label=moeda1)
+    plt.bar(x + pd.Timedelta(days=2), df_date2.values, 
+    width=3, color="blue", label=moeda2)
+    plt.show()
+    return
 
-response = requests.get(url, params=params)
-data = response.json()
-df = pd.DataFrame(data["prices"], columns=["timestamp", "price"])
-df["date"] = pd.to_datetime(df["timestamp"], unit="ms")
-df = df.set_index("date")
-
-df_month = df["price"].resample("M").last()
-print(df_month)
 
 
-plt.title(moeda+" 2025-2026")
-plt.xlabel("Mês")
-plt.ylabel("Valor em USD")
-plt.stem(df_month.index, df_month.values)
-plt.show()
+moedaConsulta1 = moedaConsulta()
+print(moedaConsulta1)
+
+
