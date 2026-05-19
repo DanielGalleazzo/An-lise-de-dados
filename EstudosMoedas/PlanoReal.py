beeae28bd271f85fc095c2cab3aa358cfc5e9ea1
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2026-05-18 23:01:35 -0300

    Particionando o gráfico

--- EstudosMoedas/PlanoReal.py
+++ EstudosMoedas/PlanoReal.py
@@ -1,3 +1,5 @@
+#https://docs.google.com/presentation/d/17LccGTbcWm21jAqUT2d8AQ5lceju32Dg9lGQKJ4vyIU/edit?slide=id.p#slide=id.p
+
 import pandas as pd
 import matplotlib.pyplot as plt
  
@@ -26,10 +28,7 @@ for col in columns_to_convert:
 
 
 df_mes = pd.DataFrame({
-    'Ultimo': df.resample('M')['Ultimo'].mean(),
-    'Abertura': df.resample('M')['Abertura'].mean(),
-    'Maxima': df.resample('M')['Maxima'].mean(),
-    'Minima': df.resample('M')['Minima'].mean()
+    'Ultimo': df.resample('ME')['Ultimo'].mean()
 })
 
 df_mes.dropna(inplace=True)
@@ -37,10 +36,76 @@ df_mes.dropna(inplace=True)
 
 plt.figure(figsize=(12, 6))
 
-plt.plot(df_mes.index, df_mes['Ultimo'], color='black', label='Média de Fechamento')
-plt.plot(df_mes.index, df_mes['Maxima'], color='red', alpha=0.5, linestyle='--', label='Média de Máximas')
-plt.plot(df_mes.index, df_mes['Minima'], color='blue', alpha=0.5, linestyle='--', label='Média de Mínimas')
-plt.plot(df_mes.index, df_mes['Abertura'], color='green', alpha=0.5, linestyle='--', label='Média de Aberturas')
+plt.plot(df_mes.index, df_mes['Ultimo'], color='black')
+
+
+plt.axvspan(
+    pd.Timestamp('1994-07-01'),
+    pd.Timestamp('1995-01-01'),
+    color='cyan',
+    alpha=0.2,
+    label='Governo Itamar'
+)
+plt.axvspan(
+    pd.Timestamp('1995-01-02'),
+    pd.Timestamp('2003-01-01'),
+    color='gray',
+    alpha=0.2,
+    label='Governo FHC'
+)
+
+plt.axvspan(
+    pd.Timestamp('2003-01-02'),
+    pd.Timestamp('2010-12-31'),
+    color='darkred',
+    alpha=0.2,
+    label='Primeiro Governo Lula'
+)
+plt.axvspan(
+    pd.Timestamp('2011-01-01'),
+    pd.Timestamp('2016-08-31'),
+    color='pink',
+    alpha=0.2,
+    label='Mandato da Dilma'
+)
+
+plt.axvspan(
+    pd.Timestamp('2016-08-31'),
+    pd.Timestamp('2018-12-31'),
+    color='blue',
+    alpha=0.2,
+    label='Mandato do Temer'
+)
+
+plt.axvspan(
+    pd.Timestamp('2019-01-01'),
+    pd.Timestamp('2022-12-31'),
+    color='green',
+    alpha=0.2,
+    label='Mandato do Bolsonaro'
+)
+
+
+
+plt.axvspan(
+    pd.Timestamp('2014-06-12'),
+    pd.Timestamp('2014-07-13'),
+    color='yellow',
+    alpha=0.2,
+    label='Copa do Mundo de 2014'
+)
+
+plt.axvspan(
+    pd.Timestamp('2023-01-01'),
+    pd.Timestamp('2026-12-31'),
+    color='darkred',
+    alpha=0.2,
+    label='Segundo Governo Lula'
+)
+
+
+
+
 
 plt.title('Média mensal do dólar')
 plt.xlabel('Mês')
