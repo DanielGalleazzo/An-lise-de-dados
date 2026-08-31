bc3f094d45b7a954e5bf9ad08b56cd0906dcb200
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2026-08-30 22:17:52 -0300

    Pronto, agora só automatizar pro e-mail

--- AlertaDeAniversário/codigo.py
+++ AlertaDeAniversário/codigo.py
@@ -10,12 +10,14 @@ df = pd.read_csv (
     low_memory= False
 )
 hoje = date.now().strftime("%d/%m")
-print(hoje)
 
 for valor in df['Aniversario']:
     print(valor)
 
-    if str(hoje) in valor:
-        print("Temos um aniversariante")
+if str(valor) == hoje:
+    aniversariante = df.loc[df['Aniversario']== hoje, 'Nome']
+    print("Feliz aniversario para: " + aniversariante.iloc[0])
+else:
+    print("Nao tem aniversariante hoje :(")  #vasco ref uber
 
   
