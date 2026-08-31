b4768be0046ea84a92a6311dd8a897d1760f8a6b
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2026-08-30 22:01:45 -0300

    Praticamente pronto

--- AlertaDeAniversário/codigo.py
+++ AlertaDeAniversário/codigo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import csv as csv
-import datetime as date
+from datetime import datetime as date
 
 df = pd.read_csv (
     'ListaAniversario.csv',
@@ -9,4 +9,13 @@ df = pd.read_csv (
     encoding= 'latin1',
     low_memory= False
 )
-print(df.head())
+hoje = date.now().strftime("%d/%m")
+print(hoje)
+
+for valor in df['Aniversario']:
+    print(valor)
+
+    if str(hoje) in valor:
+        print("Temos um aniversariante")
+
+  
